b6e8d3c4a864281e316b1bee15e3039920272143
Justin <hoggersoh@gmail.com>
2024-04-11 03:15:02 +0800

    add Integration Testing for show Latest
    - covered all printLatest positive test case
    - to do negative test cases

--- src/test/java/ui/IntegrationTest.java
+++ src/test/java/ui/IntegrationTest.java
@@ -1,6 +1,10 @@
 package ui;
 
+import constants.ErrorConstant;
+import constants.HealthConstant;
+import constants.UiConstant;
 import constants.WorkoutConstant;
+import health.Bmi;
 import health.HealthList;
 import health.Period;
 import utility.CustomExceptions;
@@ -57,6 +61,300 @@ public class IntegrationTest {
         System.setErr(originalErr);
         System.setIn(originalIn);
     }
+
+    /**
+     * Helper method to help create the output string when BMI is added
+     * @param weight weight of the user
+     * @param height height of the user
+     * @param date date of the user in the format yyyy-mm-dd
+     * @param bmiValue bmi value of the user in 2 decimal places
+     * @param bmiCategory the category (use the constants in HealthConstant)
+     * @return String representing the output
+     */
+    private String addBmiOutputString (String weight, String height, String date, double bmiValue, String bmiCategory){
+
+        StringBuilder outputString = new StringBuilder();
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+        outputString.append(HealthConstant.BMI_ADDED_MESSAGE_PREFIX);
+        outputString.append(height);
+        outputString.append(UiConstant.LINE);
+        outputString.append(weight);
+        outputString.append(UiConstant.LINE);
+        outputString.append(date);
+        outputString.append(System.lineSeparator());
+        outputString.append(String.format(HealthConstant.PRINT_BMI_FORMAT,
+                date, bmiValue, Bmi.getBmiCategory(bmiValue)));
+        outputString.append(System.lineSeparator());
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+        return outputString.toString();
+    }
+
+    /**
+     * Helper method to help create the latestString for Period
+     * @param date date of the bmi in the format yyyy-mm-dd
+     * @param bmiValue bmi value of the user in 2 decimal places
+     * @param bmiMessage the category (use the constants in HealthConstant)
+     * @return String representing the output
+     */
+    private String bmiLatestOutputString(String date, double bmiValue, String bmiMessage ) {
+        return UiConstant.PARTITION_LINE +
+                System.lineSeparator() +
+                date +
+                System.lineSeparator() +
+                "Your BMI is " +
+                String.format("%.2f" , bmiValue) +
+                System.lineSeparator() +
+                bmiMessage +
+                System.lineSeparator() +
+                UiConstant.PARTITION_LINE +
+                System.lineSeparator();
+    }
+
+    /**
+     * Helper method to help create the output string when Period is added
+     *
+     * @param startDate    start date of the period in the format yyyy-mm-dd
+     * @param endDate      end date of the period in the format yyyy-mm-dd
+     * @param periodLength length of the period in days
+     * @return String representing the output
+     */
+    private String addPeriodOutputString (String startDate,
+                                          String endDate,
+                                          int periodLength){
+
+        StringBuilder outputString = new StringBuilder();
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+        outputString.append(HealthConstant.PERIOD_ADDED_MESSAGE_PREFIX);
+        outputString.append(startDate);
+        outputString.append(UiConstant.LINE);
+        outputString.append(endDate);
+        outputString.append(System.lineSeparator());
+        outputString.append(String.format(HealthConstant.PRINT_PERIOD_FORMAT,
+                startDate,
+                endDate ,
+                periodLength,
+                HealthConstant.DAYS_MESSAGE));
+        outputString.append(System.lineSeparator());
+        if (!HealthConstant.PERIOD_TOO_LONG_MESSAGE.isBlank()) {
+            outputString.append(ErrorConstant.COLOR_HEADING +
+                    HealthConstant.PERIOD_TOO_LONG_MESSAGE +
+                    ErrorConstant.COLOR_ENDING);
+            outputString.append(System.lineSeparator());
+        }
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+
+
+        return outputString.toString();
+    }
+
+    /**
+     * Helper method to help create the latestString for Period
+     * @param startDate start date of the period in the format yyyy-mm-dd
+     * @param endDate end date of the period in the format yyyy-mm-dd
+     * @param periodLength length of the period in days
+     * @param period the "days" message (use the constants in HealthConstant)
+     * @return String representing the output
+     */
+    private String periodLatestOutputString(String startDate, String endDate, int periodLength, String period) {
+        return UiConstant.PARTITION_LINE +
+                System.lineSeparator() +
+                String.format(HealthConstant.PRINT_PERIOD_FORMAT,
+                        startDate,
+                        endDate,
+                        periodLength,
+                        period) +
+                System.lineSeparator() +
+                UiConstant.PARTITION_LINE +
+                System.lineSeparator();
+
+    }
+
+    /**
+     * Helper method to help create the output string when Appointment is added
+     * @param date date of the appointment in the format yyyy-mm-dd
+     * @param time time of the appointment in the format hh:mm
+     * @param description description of the appointment
+     * @return String representing the output
+     */
+    private String addAppointmentString(String date, String time, String description) {
+        StringBuilder outputString = new StringBuilder();
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+        outputString.append(HealthConstant.APPOINTMENT_ADDED_MESSAGE_PREFIX);
+        outputString.append(date);
+        outputString.append(UiConstant.LINE);
+        outputString.append(time);
+        outputString.append(UiConstant.LINE);
+        outputString.append(description);
+        outputString.append(System.lineSeparator());
+        outputString.append(String.format(HealthConstant.PRINT_APPOINTMENT_FORMAT, date, time, description));
+        outputString.append(System.lineSeparator());
+        outputString.append(UiConstant.PARTITION_LINE);
+        outputString.append(System.lineSeparator());
+        return outputString.toString();
+    }
+
+    /**
+     * Helper method to help create the latestString for Appointment
+     * @param date date of the appointment in the format yyyy-mm-dd
+     * @param time time of the appointment in the format hh:mm
+     * @param description description of the appointment
+     * @return String representing the output
+     */
+    private String appointmentLatestOutputString(String date, String time, String description) {
+        return UiConstant.PARTITION_LINE +
+                System.lineSeparator() +
+                String.format(HealthConstant.PRINT_APPOINTMENT_FORMAT, date, time, description) +
+                System.lineSeparator() +
+                UiConstant.PARTITION_LINE +
+                System.lineSeparator();
+    }
+
+    /**
+     * Test the behaviour of print latest when given the filter period/bmi/appointment
+     * This is the flow of sequence
+     * 1. Add a bmi (bmi1)
+     * 2. Print latest bmi (prints bmi1)
+     * 3. Add a bmi (bmi2)
+     * 4. Print latest bmi (prints bmi2) - latest bmi should be bmi2
+     * 5. Add a period (period1)
+     * 6. Print latest period (prints period1)
+     * 7. Add a period (period2)
+     * 8. Print latest period (prints period2) - latest period should be period2
+     * 9. Add an appointment (appointment1)
+     * 10. Print latest appointment (prints appointment1)
+     * 11. Add an appointment (appointment2) that is earlier than appointment1
+     * 12. Print latest appointment (prints appointment2) - latest appointment should be appointment2 as it is earlier
+     * 13. Add an appointment (appointment3) that is the latest (2026)
+     * 14. Print latest appointment (prints appointment2) - latest appointment should be appointment2 as it is earliest
+     *
+     */
+    @Test
+    void addHealth_correctInput_expectCorrectLatestOutput() {
+
+        // Craft the input String to be passed to handler
+        StringBuilder inputString = new StringBuilder();
+        inputString.append("health /h:bmi /height:1.75 /weight:70.00 /date:18-03-2024");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:bmi");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:bmi /height: 2.00 /weight:40.00 /date:20-03-2024");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:bmi");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:period /start:18-12-2023 /end:26-12-2023");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:period");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:period /start:27-01-2024 /end:03-03-2024");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:period");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:appointment /date:29-04-2025 /time:12:00 /description:knee surgery");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:appointment");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:appointment /date:25-03-2024 /time:23:01 /description:knee surgery 2");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:appointment");
+        inputString.append(System.lineSeparator());
+        inputString.append("health /h:appointment /date:25-03-2026 /time:10:01 /description:plastic surgery");
+        inputString.append(System.lineSeparator());
+        inputString.append("latest /item:appointment");
+
+
+        // Craft the expected String to be printed
+        StringBuilder expectedString = new StringBuilder();
+
+        String addBmiString = addBmiOutputString("70.00",
+                "1.75",
+                "2024-03-18",
+                22.86,
+                HealthConstant.NORMAL_WEIGHT_MESSAGE);
+
+        String latestBmiString = bmiLatestOutputString("2024-03-18",
+                22.86,
+                HealthConstant.NORMAL_WEIGHT_MESSAGE);
+
+
+        String addBmiString2 = addBmiOutputString("40.00",
+                "2.00",
+                "2024-03-20",
+                10.00,
+                HealthConstant.UNDERWEIGHT_MESSAGE);
+
+        String latestBmiString2 = bmiLatestOutputString("2024-03-20",
+                10.00,
+                HealthConstant.UNDERWEIGHT_MESSAGE);
+
+        String addPeriodString = addPeriodOutputString("2023-12-18",
+                "2023-12-26",
+                9
+        );
+
+        String latestPeriodString = periodLatestOutputString("2023-12-18",
+                "2023-12-26",
+                9,
+                HealthConstant.DAYS_MESSAGE);
+
+        String addPeriodString2 = addPeriodOutputString("2024-01-27",
+                "2024-03-03",
+                37
+        );
+
+        String latestPeriodString2 = periodLatestOutputString("2024-01-27",
+                "2024-03-03",
+                37,
+                HealthConstant.DAYS_MESSAGE);
+
+        String addAppointmentString = addAppointmentString("2025-04-29",
+                "12:00",
+                "knee surgery");
+
+        String latestAppointmentString = appointmentLatestOutputString("2025-04-29",
+                "12:00",
+                "knee surgery");
+
+        String addAppointmentString2 = addAppointmentString("2024-03-25",
+                "23:01",
+                "knee surgery 2");
+
+        String latestAppointmentString2 = appointmentLatestOutputString("2024-03-25",
+                "23:01",
+                "knee surgery 2");
+
+        String addAppointmentString3 = addAppointmentString("2026-03-25",
+                "10:01",
+                "plastic surgery");
+
+
+        expectedString.append(addBmiString);
+        expectedString.append(latestBmiString);
+        expectedString.append(addBmiString2);
+        expectedString.append(latestBmiString2);
+        expectedString.append(addPeriodString);
+        expectedString.append(latestPeriodString);
+        expectedString.append(addPeriodString2);
+        expectedString.append(latestPeriodString2);
+        expectedString.append(addAppointmentString);
+        expectedString.append(latestAppointmentString);
+        expectedString.append(addAppointmentString2);
+        expectedString.append(latestAppointmentString2); // the latest appointment is earlier so it should print first
+        expectedString.append(addAppointmentString3);
+        expectedString.append(latestAppointmentString2); // the latest appointment is earlier then appointment3
+
+        // Run the process to test the output
+        Handler handler= new Handler(inputString.toString());
+        handler.processInput();
+        assertEquals(expectedString.toString(), outContent.toString());
+        outContent.reset();
+
+    }
+
 
     /**
      * Tests the behaviour of having the same expected output when saving and loading a Gym object.
